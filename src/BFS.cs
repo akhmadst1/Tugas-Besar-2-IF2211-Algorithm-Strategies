aa7497cc2b61d46f5d31b0faefb3c8a3372f0a94
unknown <ferindyaulia19@gmail.com>
2023-03-22 21:57:25 +0700

    Add folder src and BFS

---
+++ src/BFS.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+class BFS
+{
+    private int V; 
+    private List<int>[] adj; 
+
+    // Konstruktor untuk inisialisasi jumlah vertex dan adjacency
+    public BFS(int v)
+    {
+        V = v;
+        adj = new List<int>[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new List<int>();
+    }
+
+    // Fungsi untuk menambahkan edge ke graph
+    public void AddEdge(int v, int w)
+    {
+        adj[v].Add(w);
+    }
+
+    // Fungsi BFS traversal dari source
+    public void BFS_Traversal(int s)
+    {
+        bool[] visited = new bool[V]; 
+        Queue<int> queue = new Queue<int>(); 
+
+        visited[s] = true; 
+        queue.Enqueue(s); 
+
+        while (queue.Count != 0) 
+        {
+            s = queue.Dequeue(); n
+
+            Console.Write(s + " "); 
+
+            // Kunjungi semua vertex yang bertetangga dengan vertex s
+            foreach (int i in adj[s])
+            {
+                if (!visited[i])
+                {
+                    visited[i] = true; 
+                    queue.Enqueue(i); 
+                }
+            }
+        }
+    }
